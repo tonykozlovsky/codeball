8e6f36cbd12ea970ca1199bc91352aa8a2478768
tony <anton8748125@gmail.com>
2018-12-29 02:39:48 +0300

    v18 blocking 360 tl

--- model/C.h
+++ model/C.h
@@ -38,7 +38,7 @@
 struct C {
   static model::Rules rules;
   static constexpr int MAX_SIMULATION_DEPTH = 200;
-  static constexpr double time_limit = 335.;
+  static constexpr double time_limit = 360.;
 
   static std::mt19937_64 rd;
 
