67bef6004785772e2a7eee1f29b2e26cdfd4e576
tony <anton8748125@gmail.com>
2019-01-21 00:37:46 +0300

    fixed check 6

--- model/C.h
+++ model/C.h
@@ -46,7 +46,7 @@ struct C {
   static constexpr int MAX_SIMULATION_DEPTH = 100 / TPT;
   static constexpr int MICROTICKS_PER_TICK = 100 * TPT;
   static constexpr double ball_antiflap = 0.1; // todo check
-  static constexpr double MIN_WALL_JUMP = 1;
+  static constexpr double MIN_WALL_JUMP = 15.;
 #ifdef LOCAL
   static constexpr double time_limit = 330. * 1.5;
 #else
